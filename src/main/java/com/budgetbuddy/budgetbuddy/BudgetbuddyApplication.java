dc0b95d16ff086effad2484cea9c3f2053fc2eef
cbrooks-uva <nug4sp@virginia.edu>
2025-03-13 11:30:07 -0400

    Initial commit

---
+++ src/main/java/com/budgetbuddy/budgetbuddy/BudgetbuddyApplication.java
@@ -0,0 +1,13 @@
+package com.budgetbuddy.budgetbuddy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BudgetbuddyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BudgetbuddyApplication.class, args);
+	}
+
+}
