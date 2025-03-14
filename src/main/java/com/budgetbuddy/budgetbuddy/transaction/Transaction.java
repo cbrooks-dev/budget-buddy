642ee4b08bebf0f21f78556e76a105a6e64253b3
cbrooks-uva <nug4sp@virginia.edu>
2025-03-13 20:07:17 -0400

    Marked Id attribute for Transaction

--- src/main/java/com/budgetbuddy/budgetbuddy/transaction/Transaction.java
+++ src/main/java/com/budgetbuddy/budgetbuddy/transaction/Transaction.java
@@ -1,11 +1,12 @@
 package com.budgetbuddy.budgetbuddy.transaction;
 
 import jakarta.validation.constraints.Positive;
+import org.springframework.data.annotation.Id;
 
 import java.time.LocalDate;
 
 public record Transaction(
-        @Positive
+        @Positive @Id
         Integer id,
         TransactionType transactionType,
         TransactionCategory transactionCategory,
