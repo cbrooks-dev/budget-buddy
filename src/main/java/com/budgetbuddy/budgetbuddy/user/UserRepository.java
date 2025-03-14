e33707f660ce8c8e2baf12f11d5726aaacd56ad7
cbrooks-dev <connorbrooks04@yahoo.com>
2025-03-14 11:28:52 -0400

    Created user repo

---
+++ src/main/java/com/budgetbuddy/budgetbuddy/user/UserRepository.java
@@ -0,0 +1,5 @@
+package com.budgetbuddy.budgetbuddy.user;
+
+import org.springframework.data.repository.ListCrudRepository;
+
+public interface UserRepository extends ListCrudRepository<User, Integer> {}
