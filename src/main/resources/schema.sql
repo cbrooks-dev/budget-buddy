96a9a1fe530e2505a615a3a27ae9ee3a6c310f43
cbrooks-dev <connorbrooks04@yahoo.com>
2025-03-13 23:47:55 -0400

    Fixed minor bug

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -2,7 +2,7 @@ CREATE TABLE IF NOT EXISTS Transaction (
     id INT NOT NULL,
     transaction_type VARCHAR(50) NOT NULL,
     transaction_category VARCHAR(50) NOT NULL,
-    amount DOUBLE NOT NULL,
+    amount DOUBLE PRECISION NOT NULL,
     date DATE NOT NULL,
     version INT,
     PRIMARY KEY (id)
