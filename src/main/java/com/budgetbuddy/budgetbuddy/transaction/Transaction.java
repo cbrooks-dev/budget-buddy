d9bfc0ae41fd4177f1d6a42ebb2a7c9dcab12fe6
cbrooks-dev <connorbrooks04@yahoo.com>
2025-03-14 11:27:49 -0400

    Added additional validation

--- src/main/java/com/budgetbuddy/budgetbuddy/transaction/Transaction.java
+++ src/main/java/com/budgetbuddy/budgetbuddy/transaction/Transaction.java
@@ -1,5 +1,6 @@
 package com.budgetbuddy.budgetbuddy.transaction;
 
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.annotation.Version;
@@ -7,12 +8,16 @@ import org.springframework.data.annotation.Version;
 import java.time.LocalDate;
 
 public record Transaction(
-        @Positive @Id
+        @Positive @Id @NotNull
         Integer id,
+        @NotNull
         TransactionType transactionType,
+        @NotNull
         TransactionCategory transactionCategory,
+        @NotNull
         Double amount,
+        @NotNull
         LocalDate date,
-        @Version
+        @Version @NotNull
         Integer version
 ) {}
